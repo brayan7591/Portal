0c98e6ba27d0274629bf602f7ccd97454221e295
Bryan <brayan7camilo@gmail.com>
2024-01-29 17:36:29 -0500

    Quinta subida

---
+++ resources/views/Page/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Home')
+
+@section('content')
+    <h1 class="text-center">El comienzo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit minus sit doloremque quaerat labore architecto cum, corporis et consequatur sunt, esse maxime perferendis, voluptas ratione tenetur ab veniam. Molestiae, nulla.</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,24 +7,27 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'titulo')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/stylemenu.css')}}" rel="stylesheet" >
+    @yield('links')
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    PortalWeb
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -75,9 +78,74 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <article>
+            <div class="fondo">
+                <ul class="menu">
+                    <div class="toggle"><i class="fa-solid fa-plus"></i></div>
+
+                    <li class="proyectos" style="--i:0;--clr:#F5FF00" title="inicio">
+                        <a href="{{route('landingPage')}}" {{request()->routeIs('landingPage') ? 'class=disabled' : ''}}><i class="fa-solid fa-house"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:1;--clr:#E2FF00" title="Curriculum">
+                        <a href="#"><i class="fa-solid fa-address-card"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:2;--clr:#2BFF00" title="Instructores">
+                        <a href="#"><i class="fa-solid fa-users"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:3;--clr:#00FFF3" title="Egresados">
+                        <a href="#"><i class="fa-solid fa-user-plus"></i></a><
+                    </li>
+
+                    <li class="proyectos" style="--i:4;--clr:#00FFF3" title="Eventos y Galerias">
+                        <a href="#"><i class="fa-solid fa-calendar-days"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:5;--clr:#00C1FC" title="Reglamento">
+                        <a href="#"><i class="fa-solid fa-book"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:6;--clr:#fff" title="Aprendices">
+                        <a href="#"><i class="fa-solid fa-medal"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:7;--clr:#ffff00" title="Proyectos">
+                        <a href="#"><i class="fa-solid fa-diagram-project"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:8;--clr:#ccc" title="Biblioteca">
+                        <a href="#"><i class="fa-sharp fa-solid fa-landmark"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:9;--clr:#FF0E43" title="Estadisticas">
+                        <a href="#"><i class="fa-solid fa-chart-line"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:10;--clr:#FF6961" title="Voceros">
+                        <a href="#"><i class="fa-solid fa-user-tie"></i></a>
+                    </li>
+
+                    <li class="proyectos" style="--i:11;--clr:#FB00FC" title="Competencias">
+                        <a href="#"><i class="fa-regular fa-circle-check"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </article>
+
+        <main>
             @yield('content')
         </main>
     </div>
+
+    <script>
+		let toggle = document.querySelector('.toggle');
+		let menu = document.querySelector('.menu');
+		toggle.onclick =function(){
+			menu.classList.toggle('active');
+		}
+	</script>
+
 </body>
 </html>
